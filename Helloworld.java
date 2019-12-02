bee516aa4633eff3f15c43ae0c95b7c46649ba28
Alakasabat <sabat.alaka6071@gmail.com>
2019-12-02 10:29:24 +0000

    All Good

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
-public class HelloWorld {
+public class Helloworld {
     public static void main(String[] args) {
-         System.out.println("Hello World!");
+         System.out.println("Hello!");
         System.out.println("Hello World!"); 
         System.out.println("Hello World! Nice Day we have..."); 
     }
